9d84e2570d2393c532f1660fce8e6b5f529c5675
App Academy Student <student@appacademy.io>
2017-02-09 14:33:10 -0800

    specify correct object id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 ActiveRecord::Base.transaction do
   User.destroy_all
-  User.create!(name: 'Wade')
-  User.create!(name: 'Hrothgar')
+  u1 = User.create(name: 'Wade')
+  u2 = User.create(name: 'Hrothgar')
 
   Poll.destroy_all
-  Poll.create!(title: 'Favorite color?', user_id:1)
+  p1 = Poll.create(title: 'Favorite color?', user_id: u1.id)
+
+  Question.destroy_all
+  q1 = Question.create(body: 'Red or blue?', poll_id: p1.id)
+  q2 = Question.create(body: 'Green or yellow?', poll_id: p1.id)
 end
