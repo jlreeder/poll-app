794cec4aca82067844254ab2de1db35958187069
App Academy Student <student@appacademy.io>
2017-02-09 13:59:36 -0800

    add annotations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: polls
+#
+#  id      :integer          not null, primary key
+#  title   :string           not null
+#  user_id :integer
+#
+
 class Poll < ActiveRecord::Base
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id   :integer          not null, primary key
+#  name :string           not null
+#
+
 class User < ActiveRecord::Base
 
 end
