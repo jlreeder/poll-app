ed9263575dbdd708e51ca2375b10c83a9196a08d
App Academy Student <student@appacademy.io>
2017-02-09 15:08:01 -0800

    add associations and validations

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -14,4 +14,9 @@ class AnswerChoice < ActiveRecord::Base
     primary_key: :id,
     foreign_key: :question_id,
     class_name: :Question
+
+  has_many :responses,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :Response
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@
 
 class User < ActiveRecord::Base
   has_many :authored_polls,
-    primary_key: :id ,
-    foreign_key: :user_id ,
+    primary_key: :id,
+    foreign_key: :user_id,
     class_name: :Poll
+
+  has_many :responses,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Response
+
+  validates :name, presence: true, uniqueness: true
 end
