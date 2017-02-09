07206fb0cb77d69ea2e29cd1461749c8a355cbf4
App Academy Student <student@appacademy.io>
2017-02-09 14:21:52 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  User.destroy_all
+  User.create!(name: 'Wade')
+  User.create!(name: 'Hrothgar')
+
+  Poll.destroy_all
+  Poll.create!(title: 'Favorite color?', user_id:1)
+end
